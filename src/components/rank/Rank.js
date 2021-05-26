288764026de788ceb71ed3861b8923e73b0b0ce7
LxrdRU <a981203@mail.ru>
2021-05-25 20:04:40 -0700

    srabotai

--- src/components/rank/Rank.js
+++ src/components/rank/Rank.js
@@ -5,10 +5,10 @@ const Rank = () => {
 	return(
 		<div>
 			<div className='white f3'>
-				{'Andre, your current rank is...'}
+			
 			</div>
 			<div className='white f1'>
-				{'#5'}
+				
 			</div>
 		</div>
 	);
